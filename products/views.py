b5baf85c5c7dbd55c5a3b9eae9a051bb07606f8c
zholudeva.e <ekaterinaadv@gmail.com>
2025-08-29 08:58:54 +0300

    implemented product detail view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from products.models import Product, Review
 
-# Create your views here.
+class ProductDetailView(DetailView):
+    slug_field = 'slug'
+    model = Product
+    template_name = 'products/product-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(product=self.object)
+        return context
