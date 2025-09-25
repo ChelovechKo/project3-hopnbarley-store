0096ea4ea2fd33414ffc22d0617db1fdf1982c91
zholudeva.e <ekaterinaadv@gmail.com>
2025-09-25 17:03:36 +0300

    fixed login success url and added user buttons

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # TODO: wrong success url
     path('register/', UserCreateView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html',
-                                     next_page=reverse_lazy('users:home'),
+                                     next_page=reverse_lazy('products:product-list'),
                                      ), name='login'),
     path('account/', AccountView.as_view(), name='account'),
 ]
