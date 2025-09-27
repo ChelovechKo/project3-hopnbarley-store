2b07917407501d0d43eaf0a413f6d622bc51037c
zholudeva.e <ekaterinaadv@gmail.com>
2025-09-27 14:04:12 +0300

    updated account.html and views.py for show order's list and added pagination

--- users/views.py
+++ users/views.py
@@ -1,27 +1,41 @@
 from django.views.generic import CreateView, TemplateView, UpdateView
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator
 from .forms import UserRegistrationForm, UserUpdateForm
 from .mixins import LoginRequiredMixin
 from .models import User
 
 
 class UserCreateView(CreateView):
+    """Регистрация пользователя"""
     form_class = UserRegistrationForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
 
 class AccountView(LoginRequiredMixin, TemplateView):
+    """Список заказов и информация об аккаунте"""
     template_name = 'users/account.html'
+    allow_empty = True
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = UserUpdateForm(instance=self.request.user)
-        context['orders'] = self.request.user.orders.all()
+
+        orders_qs = self.request.user.orders.order_by('-create_at')
+        paginator = Paginator(orders_qs, 10)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context['page_obj'] = page_obj
+        context['is_paginated'] = page_obj.has_other_pages()
+        context['paginator'] = paginator
+        context['orders'] = page_obj.object_list
         return context
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
+    """Обновление информации в аккаунте"""
     queryset = User.objects.all()
     form_class = UserUpdateForm
     success_url = reverse_lazy('users:account')
